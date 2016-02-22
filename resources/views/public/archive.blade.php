f3cbba37b7e810e8d0282ad33c14410a5b45e5e0
mammutcris <mammutcris@gmail.com>
2016-02-22 14:49:39 +0200

    Виведено архів на сайт

--- resources/views/public/archive.blade.php
+++ resources/views/public/archive.blade.php
@@ -6,54 +6,27 @@
 
     <div class="container">
         <ol class="breadcrumb">
-            <li><a href="index.html">Головна</a></li>
+            <li><a href="/">Головна</a></li>
             <li class="active">Архів</li>
         </ol>
         <h1 class="thin">Архів</h1>
+        @foreach($archives as $archive)
+            <div class="jumbotron jumbotron-archive top-space">
+                <h2>15 Конференція</h2>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <img src="{{$archive->photo_thesis}}" alt="">
+                        <p class="text-right"><a class="btn btn-success btn-lg btn-block" href="{{$archive->thesis}}">Завантажити
+                                тези</a></p>
+                    </div>
+                    <div class="col-sm-6">
+                        <img src="{{$archive->photo_img}}" alt="">
+                        <p class="text-right"><a class="btn btn-primary btn-lg btn-block" href="{{$archive->img}}">Завантажити
+                                фото</a></p>
+                    </div>
+                </div>
 
-        <div class="jumbotron jumbotron-archive top-space">
-            <h2>15 Конференція</h2>
-            <div class="row">
-                <div class="col-sm-6">
-                    <h3>Dicta, nostrum nemo soluta sapiente sit dolor quae voluptas quidem doloribus recusandae facere
-                        magni
-                        ullam suscipit sunt atque rerum eaque iusto facilis esse nam veniam incidunt officia
-                        perspiciatis at
-                        voluptatibus. Libero, aliquid illum possimus numquam fuga.</h3>
-                    <p class="text-right"><a class="btn btn-success btn-lg btn-block">Завантажити тези</a></p>
-                </div>
-                <div class="col-sm-6">
-                    <h3>Dicta, nostrum nemo soluta sapiente sit dolor quae voluptas quidem doloribus recusandae facere
-                        magni
-                        ullam suscipit sunt atque rerum eaque iusto facilis esse nam veniam incidunt officia
-                        perspiciatis at
-                        voluptatibus. Libero, aliquid illum possimus numquam fuga.</h3>
-                    <p class="text-right"><a class="btn btn-primary btn-lg btn-block">Завантажити фото</a></p>
-                </div>
             </div>
-
-        </div>
-        <div class="jumbotron jumbotron-archive top-space">
-            <h2>14 Конференція</h2>
-            <div class="row">
-                <div class="col-sm-6">
-                    <h3>Dicta, nostrum nemo soluta sapiente sit dolor quae voluptas quidem doloribus recusandae facere
-                        magni
-                        ullam suscipit sunt atque rerum eaque iusto facilis esse nam veniam incidunt officia
-                        perspiciatis at
-                        voluptatibus. Libero, aliquid illum possimus numquam fuga.</h3>
-                    <p class="text-right"><a class="btn btn-success btn-lg btn-block">Завантажити тези</a></p>
-                </div>
-                <div class="col-sm-6">
-                    <h3>Dicta, nostrum nemo soluta sapiente sit dolor quae voluptas quidem doloribus recusandae facere
-                        magni
-                        ullam suscipit sunt atque rerum eaque iusto facilis esse nam veniam incidunt officia
-                        perspiciatis at
-                        voluptatibus. Libero, aliquid illum possimus numquam fuga.</h3>
-                    <p class="text-right"><a class="btn btn-primary btn-lg btn-block">Завантажити фото</a></p>
-                </div>
-            </div>
-        </div>
     </div>
-
+    @endforeach
 @endsection
