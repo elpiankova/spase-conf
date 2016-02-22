df7eef1a70fcd831446ee2d78a1a639b7371ed64
mammutcris <mammutcris@gmail.com>
2016-02-22 09:05:43 +0200

    Зроблено приклад редагування сайта

--- app/Admin/menu.php
+++ app/Admin/menu.php
@@ -1,5 +1,26 @@
 <?php
 
+Admin::menu()->label('Сайт')->icon('fa-desktop')->items(function ()
+{
+    Admin::menu()->label('Головна')->icon('fa-desktop')->items(function ()
+    {
+        Admin::menu(App\User::class)->label('Слайд')->icon('fa-key');
+        Admin::menu(App\User::class)->label('Сторінка')->icon('fa-graduation-cap');
+        Admin::menu(App\User::class)->label('Рубрики')->icon('fa-user');
+    });
+
+    Admin::menu(App\User::class)->label('Архів')->icon('fa-key');
+
+    Admin::menu()->label('Конференція')->icon('fa-desktop')->items(function ()
+    {
+        Admin::menu(App\User::class)->label('Налаштування')->icon('fa-key');
+        Admin::menu(App\User::class)->label('Інформація')->icon('fa-graduation-cap');
+        Admin::menu(App\User::class)->label('Тези')->icon('fa-user');
+        Admin::menu(App\User::class)->label('Місце проведення')->icon('fa-user');
+        Admin::menu(App\User::class)->label('Комітет')->icon('fa-user');
+    });
+});
+
 Admin::menu()->label('Пользователи')->icon('fa-users')->items(function ()
 {
     Admin::menu(App\Permit::class)->label('Права')->icon('fa-key');
