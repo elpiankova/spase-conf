d72a12f4db6003775a20af2768bb11d5ffc9baa0
mammutcris <mammutcris@gmail.com>
2016-05-02 19:05:47 +0300

    Виправлено вигляд паролю

--- resources/views/widgets/form/_formitem_password.blade.php
+++ resources/views/widgets/form/_formitem_password.blade.php
@@ -8,6 +8,8 @@
 <?php if (!isset($value)) $value = null ?>
 <div class="{!! $errors->has($name) ? 'has-error' : null !!} top-margin">
     <label for="{!! $name !!}">{{ $title }}</label>
-    <input type="password" name="$name" value="$value" placeholder="$placeholder" class="form-control text_form">
+    {{--{!! Form::password($name, $value, ['class' => 'form-control text_form']) !!}--}}
+    {{--<input type="password" name="$name" value="$value" placeholder="$placeholder" class="form-control text_form">--}}
+    <input name="password" type="password" value="$value"  placeholder="$placeholder" class="form-control text_form">
     <p class="help-block">{!! $errors->first($name) !!}</p>
 </div>
