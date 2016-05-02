fa3ce3a05e4bbf2fa6ef25899b13a70553768580
mammutcris <mammutcris@gmail.com>
2016-05-02 16:50:57 +0300

    Зроблено форми

--- resources/views/widgets/form/_formitem_password.blade.php
+++ resources/views/widgets/form/_formitem_password.blade.php
@@ -1,13 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mammut
- * Date: 21.02.16
- * Time: 16:37
- */?>
-<?php if(! isset($value)) $value = null ?>
-<div class="{!! $errors->has($name) ? 'has-error' : null !!}">
+    /**
+     * Created by PhpStorm.
+     * User: mammut
+     * Date: 21.02.16
+     * Time: 16:37
+     */ ?>
+<?php if (!isset($value)) $value = null ?>
+<div class="{!! $errors->has($name) ? 'has-error' : null !!} top-margin">
     <label for="{!! $name !!}">{{ $title }}</label>
-    {!! Form::password($name, $value, array('placeholder' =>  $placeholder )) !!}
+    <input type="password" name="$name" value="$value" placeholder="$placeholder" class="form-control text_form">
     <p class="help-block">{!! $errors->first($name) !!}</p>
 </div>
--- resources/views/widgets/form/_formitem_text.blade.php
+++ resources/views/widgets/form/_formitem_text.blade.php
@@ -1,13 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mammut
- * Date: 21.02.16
- * Time: 16:37
- */?>
-<?php if(! isset($value)) $value = null ?>
-<div class="{!! $errors->has($name) ? 'has-error' : null !!}">
+    /**
+     * Created by PhpStorm.
+     * User: mammut
+     * Date: 21.02.16
+     * Time: 16:37
+     */ ?>
+<?php if (!isset($value)) $value = null ?>
+<div class="{!! $errors->has($name) ? 'has-error' : null !!} top-margin">
     <label for="{!! $name !!}">{{ $title }}</label>
-    {!! Form::text($name, $value, array('placeholder' =>  $placeholder )) !!}
+    {!! Form::text($name, $value, array('placeholder' =>  $placeholder, 'class'=>'form-control text_form')) !!}
     <p class="help-block">{!! $errors->first($name) !!}</p>
 </div>
