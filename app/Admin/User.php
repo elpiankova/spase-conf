63bf21e401ac87548eec4733a8097326931e334b
mammutcris <mammutcris@gmail.com>
2016-05-14 11:27:28 +0300

    Робоче відображення

--- app/Admin/User.php
+++ app/Admin/User.php
@@ -19,7 +19,6 @@ Admin::model('App\User')->title('Пользователи')->display(function() 
     $form=AdminForm::form();
     $form->items([
         FormItem::text('email', 'Email'),
-        FormItem::timestamp('last_login', 'Last Login')->format('d.m.Y'),//->seconds(true),
         FormItem::text('first_name', 'First Name'),
         FormItem::text('last_name', 'Last Name'),
         FormItem::multiselect('theroles', 'Роли')->model('App\Role')->display('name'),
