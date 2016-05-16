34eb41b2946d280cfbf476b6e325090e462d6955
mammutcris <mammutcris@gmail.com>
2016-05-16 14:35:15 +0300

    Автозаливання

---
+++ public/deploy.php
@@ -0,0 +1,24 @@
+<?php
+    /**
+     * GIT DEPLOYMENT SCRIPT
+     *
+     * Used for automatically deploying websites via GitHub
+     *
+     */
+
+    // array of commands
+    $commands = [
+        'echo $PWD',
+        'whoami',
+        'git pull',
+        'git status',
+        'git submodule sync',
+        'git submodule update',
+        'git submodule status',
+    ];
+
+    // exec commands
+    $output = '';
+    foreach ($commands AS $command) {
+        $tmp = shell_exec($command);
+    }
