069742b4198c22ad793c5cb6a26c1be456a53dff
mammutcris <mammutcris@gmail.com>
2016-05-29 21:29:54 +0300

    Зроблено міграцію

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,10 @@
     namespace App\Http\Controllers;
 
     use AdminSection;
-    use App\User;
     use App\Http\Requests;
+    use App\User;
+    use App\UserInfo;
+    use Illuminate\Support\Facades\Log;
 
     class AdminController extends Controller
     {
@@ -18,7 +20,22 @@
         public
         function migrate()
         {
-            $user = User::get();
-            dd($user);
+            $users = User::get();
+            foreach ($users as $user) {
+                $user_info = UserInfo::where('user_id', '=', $user->id)->first();
+//                dd($user_info);
+                $user->middle_name     = $user_info->middle_name;
+                $user->birth           = $user_info->birth;
+                $user->organization_id = $user_info->organization_id;
+                $user->country_id      = $user_info->country_id;
+                $user->city_id         = $user_info->city_id;
+                $user->phone           = $user_info->phone;
+                $user->photo           = $user_info->photo;
+                $user->gender          = $user_info->gender;
+                $user->save();
+                $log = $user->middle_name . $user->first_name . $user->last_name . ' з email :' . $user->email . ' перенесено';
+                Log::info($user->id, ['context' => $log]);
+
+            }
         }
     }
