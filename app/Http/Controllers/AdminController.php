1e90038bc050bb53cb3e2882a3311fee32d4d6a8
mammut <mammutcris@gmail.com>
2016-07-06 16:15:08 +0300

    Created script for deleted duplication organisation

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,97 +1,117 @@
 <?php
 
-    namespace App\Http\Controllers;
+	namespace App\Http\Controllers;
 
-    use AdminSection;
-    use App\Conferauthor;
-    use App\Conferequest;
-    use App\Http\Requests;
-    use App\User;
-    use App\UserInfo;
-    use App\Сategory;
-    use Illuminate\Support\Facades\Input;
-    use Illuminate\Support\Facades\Log;
-    use SleepingOwl\Admin\Http\Controllers\AdminController as SleepingOwl;
+	use AdminSection;
+	use App\Conferauthor;
+	use App\Conferequest;
+	use App\Http\Requests;
+	use App\Spaceorganization;
+	use App\User;
+	use App\UserInfo;
+	use App\Сategory;
+	use Illuminate\Support\Facades\Input;
+	use Illuminate\Support\Facades\Log;
+	use SleepingOwl\Admin\Http\Controllers\AdminController as SleepingOwl;
 
-    class AdminController extends SleepingOwl
-    {
-        public
-        function index()
-        {
-            $content = 'ok';
+	class AdminController extends SleepingOwl
+	{
+		public
+		function index()
+		{
+			$content = 'ok';
 
-            return $this->renderContent(view('auth.login'));
-        }
+			return $this->renderContent(view('auth.login'));
+		}
 
-        public
-        function request()
-        {
-            $contents   = Conferequest::where('status', '!=', '1')->get();
-            $categories = Сategory::orderBy('place')->get();
-            $select     = ['title' => true, 'author' => true, 'email' => true, 'text' => true, 'organization' => 'all'];
+		public
+		function request()
+		{
+			$contents = Conferequest::where('status', '!=', '1')->get();
+			$categories = Сategory::orderBy('place')->get();
+			$select = ['title' => true, 'author' => true, 'email' => true, 'text' => true, 'organization' => 'all'];
 
-            return $this->renderContent(view('admin.request', ['contents' => $contents, 'categories' => $categories, 'id_category' => $select]));
-        }
+			return $this->renderContent(view('admin.request', ['contents' => $contents, 'categories' => $categories, 'id_category' => $select]));
+		}
 
-        public
-        function requestPost()
-        {
-            $all = Input::all();
-            if ($all['organization'] == 'all') {
-                $contents = Conferequest::where('status', '!=', '1')->get();
-            } else {
-                $contents = Conferequest::where('status', '!=', '1')->where('section_id', $all['organization'])->get();
-            }
+		public
+		function requestPost()
+		{
+			$all = Input::all();
+			if ($all['organization'] == 'all') {
+				$contents = Conferequest::where('status', '!=', '1')->get();
+			} else {
+				$contents = Conferequest::where('status', '!=', '1')->where('section_id', $all['organization'])->get();
+			}
 
-            $categories = Сategory::orderBy('place')->get();
+			$categories = Сategory::orderBy('place')->get();
 
-            return $this->renderContent(view('admin.request', ['contents' => $contents, 'categories' => $categories, 'id_category' => $all]));
-        }
-        public
-        function migrate()
-        {
-            $users = User::get();
-            foreach ($users as $user) {
-                $user_info = UserInfo::where('user_id', '=', $user->id)->first();
+			return $this->renderContent(view('admin.request', ['contents' => $contents, 'categories' => $categories, 'id_category' => $all]));
+		}
+
+		public
+		function migrate()
+		{
+			$users = User::get();
+			foreach ($users as $user) {
+				$user_info = UserInfo::where('user_id', '=', $user->id)->first();
 //                dd($user_info);
-                $user->middle_name     = $user_info->middle_name;
-                $user->birth           = $user_info->birth;
-                $user->organization_id = $user_info->organization_id;
-                $user->country_id      = $user_info->country_id;
-                $user->city_id         = $user_info->city_id;
-                $user->phone           = $user_info->phone;
-                $user->photo           = $user_info->photo;
-                $user->gender          = $user_info->gender;
-                $user->save();
-                $log = $user->middle_name . $user->first_name . $user->last_name . ' з email :' . $user->email . ' перенесено';
-                Log::info($user->id, ['context' => $log]);
+				$user->middle_name = $user_info->middle_name;
+				$user->birth = $user_info->birth;
+				$user->organization_id = $user_info->organization_id;
+				$user->country_id = $user_info->country_id;
+				$user->city_id = $user_info->city_id;
+				$user->phone = $user_info->phone;
+				$user->photo = $user_info->photo;
+				$user->gender = $user_info->gender;
+				$user->save();
+				$log = $user->middle_name . $user->first_name . $user->last_name . ' з email :' . $user->email . ' перенесено';
+				Log::info($user->id, ['context' => $log]);
 
-            }
-        }
+			}
+		}
 
-        public
-        function author()
-        {
-            $autors = Conferauthor::get();
-            foreach ($autors as $autor) {
-                $reqests = Conferequest::where('user_id', $autor->conferequests_id)->get();
-                foreach ($reqests as $reqest) {
-                    $autor->conferequests_id = $reqest->id;
-                    $autor->save();
-                }
-            }
+		public
+		function author()
+		{
+			$autors = Conferauthor::get();
+			foreach ($autors as $autor) {
+				$reqests = Conferequest::where('user_id', $autor->conferequests_id)->get();
+				foreach ($reqests as $reqest) {
+					$autor->conferequests_id = $reqest->id;
+					$autor->save();
+				}
+			}
 
-            return redirect('home');
-        }
+			return redirect('home');
+		}
 
-        public
-        function section()
-        {
-            $sections = Conferequest::where('status', 0)->get();
-            foreach ($sections as $section) {
-                $section->section_id = $section->section_id + 1;
-                $section->save();
-            }
-            return redirect('home');
-        }
-    }
+		public
+		function section()
+		{
+			$sections = Conferequest::where('status', 0)->get();
+			foreach ($sections as $section) {
+				$section->section_id = $section->section_id + 1;
+				$section->save();
+			}
+
+			return redirect('home');
+		}
+
+		public function clear_organisation()
+		{
+			$organisations = Spaceorganization::all();
+			foreach ($organisations as $organisation) {
+				$identicals = Spaceorganization::where('title_uk', '=', $organisation->title_uk)->get();
+				foreach ($identicals as $key => $identical) {
+					if ($key == 0) {
+						$first_id = $identical->id;
+					} else {
+						User::where('organization_id', '=', $identical->id)->update(['organization_id' => $first_id]);
+						Conferauthor::where('organization_id', '=', $identical->id)->update(['organization_id' => $first_id]);
+						$identical->delete();
+					}
+				}
+			}
+		}
+	}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,95 +1,96 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+	/*
+	|--------------------------------------------------------------------------
+	| Routes File
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you will register all of the routes in an application.
+	| It's a breeze. Simply tell Laravel the URIs it should respond to
+	| and give it the controller to call when that URI is requested.
+	|
+	*/
 
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
+	/*
+	|--------------------------------------------------------------------------
+	| Application Routes
+	|--------------------------------------------------------------------------
+	|
+	| This route group applies the "web" middleware group to every route
+	| it contains. The "web" middleware group is defined in your HTTP
+	| kernel and includes session state, CSRF protection, and more.
+	|
+	*/
 
-Route::group(['middleware' => ['web']], function () {
+	Route::group(['middleware' => ['web']], function () {
 
 
-    Route::get('lang/{lang}', ['as' => 'lang.switch', 'uses' => 'LanguageController@switchLang']);
-    
-    Route::get('/', 'WelcomeController@index');
-    Route::get('/archive', 'WelcomeController@archive');
-    Route::get('/contact', 'WelcomeController@contact');
-    Route::post('/contact', 'WelcomeController@contactpost');
+		Route::get('lang/{lang}', ['as' => 'lang.switch', 'uses' => 'LanguageController@switchLang']);
 
-    Route::get('/conference/info', 'WelcomeController@confInfo');
-    Route::get('/conference/thesis', 'WelcomeController@confThesis');
-    Route::get('/conference/place', 'WelcomeController@confPlace');
-    Route::get('/conference/committee', 'WelcomeController@confCommittee');
-    // Вызов страницы регистрации пользователя
-    Route::get('register', 'AuthController@register');
+		Route::get('/', 'WelcomeController@index');
+		Route::get('/archive', 'WelcomeController@archive');
+		Route::get('/contact', 'WelcomeController@contact');
+		Route::post('/contact', 'WelcomeController@contactpost');
+
+		Route::get('/conference/info', 'WelcomeController@confInfo');
+		Route::get('/conference/thesis', 'WelcomeController@confThesis');
+		Route::get('/conference/place', 'WelcomeController@confPlace');
+		Route::get('/conference/committee', 'WelcomeController@confCommittee');
+		// Вызов страницы регистрации пользователя
+		Route::get('register', 'AuthController@register');
 // Пользователь заполнил форму регистрации и отправил
-    Route::post('register', 'AuthController@registerProcess');
+		Route::post('register', 'AuthController@registerProcess');
 // Пользователь получил письмо для активации аккаунта со ссылкой сюда
-    Route::get('activate/{id}/{code}', 'AuthController@activate');
+		Route::get('activate/{id}/{code}', 'AuthController@activate');
 // Вызов страницы авторизации
-    Route::get('login', 'AuthController@login');
+		Route::get('login', 'AuthController@login');
 // Пользователь заполнил форму авторизации и отправил
-    Route::post('login', 'AuthController@loginProcess');
+		Route::post('login', 'AuthController@loginProcess');
 // Выход пользователя из системы
-    Route::get('logout', 'AuthController@logoutuser');
+		Route::get('logout', 'AuthController@logoutuser');
 // Пользователь забыл пароль и запросил сброс пароля. Это начало процесса -
 // Страница с запросом E-Mail пользователя
-    Route::get('reset', 'AuthController@resetOrder');
+		Route::get('reset', 'AuthController@resetOrder');
 // Пользователь заполнил и отправил форму с E-Mail в запросе на сброс пароля
-    Route::post('reset', 'AuthController@resetOrderProcess');
+		Route::post('reset', 'AuthController@resetOrderProcess');
 // Пользователю пришло письмо со ссылкой на эту страницу для ввода нового пароля
-    Route::get('reset/{id}/{code}', 'AuthController@resetComplete');
+		Route::get('reset/{id}/{code}', 'AuthController@resetComplete');
 // Пользователь ввел новый пароль и отправил.
-    Route::post('reset/{id}/{code}', 'AuthController@resetCompleteProcess');
+		Route::post('reset/{id}/{code}', 'AuthController@resetCompleteProcess');
 // Сервисная страничка, показываем после заполнения рег формы, формы сброса и т.
 // о том, что письмо отправлено и надо заглянуть в почтовый ящик.
-    Route::get('wait', 'AuthController@wait');
+		Route::get('wait', 'AuthController@wait');
 
-    Route::get('phpinfo', function(){
-       phpinfo();
-    });
-    Route::get('gitpull', function () {
-        system('git pull');
-    });
-});
+		Route::get('phpinfo', function () {
+			phpinfo();
+		});
+		Route::get('gitpull', function () {
+			system('git pull');
+		});
+	});
 
-Route::group(['middleware' => ['web', 'authuser']], function () {
+	Route::group(['middleware' => ['web', 'authuser']], function () {
 //    Головна сторінка користувача
-    Route::get('home', 'UserController@home');
+		Route::get('home', 'UserController@home');
 //    Редагування особистих даних
-    Route::get('home/edit', 'UserController@edit');
-    Route::post('home/edit', 'UserController@editProcess');
+		Route::get('home/edit', 'UserController@edit');
+		Route::post('home/edit', 'UserController@editProcess');
 //    Редагування email
-    Route::get('home/email', 'UserController@email');
-    Route::post('home/email', 'UserController@emailProcess');
+		Route::get('home/email', 'UserController@email');
+		Route::post('home/email', 'UserController@emailProcess');
 //    Редагування паролю
-    Route::get('home/pass', 'UserController@pass');
-    Route::post('home/pass', 'UserController@passProcess');
+		Route::get('home/pass', 'UserController@pass');
+		Route::post('home/pass', 'UserController@passProcess');
 //    Заявка на конференцію
-    Route::get('home/conf', 'UserController@conf');
-    Route::post('home/conf', 'UserController@confProcess');
+		Route::get('home/conf', 'UserController@conf');
+		Route::post('home/conf', 'UserController@confProcess');
 //    Заявка на конференцію як слухач
-    Route::get('home/conf_user', 'UserController@confUserProcess');
-});
-    Route::group(['middleware' => ['web', 'isadmin']], function () {
-        Route::get('migrate', 'AdminController@migrate');
-        Route::get('config_author', 'AdminController@author');
-        Route::get('config_section', 'AdminController@section');
-    });
+		Route::get('home/conf_user', 'UserController@confUserProcess');
+	});
+	Route::group(['middleware' => ['web', 'isadmin']], function () {
+		Route::get('migrate', 'AdminController@migrate');
+		Route::get('config_author', 'AdminController@author');
+		Route::get('config_section', 'AdminController@section');
+		Route::get('clear_organisation', 'AdminController@clear_organisation');
+	});
